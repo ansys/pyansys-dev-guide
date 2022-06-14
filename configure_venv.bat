4faf464be9fd2ad32b6f3387a653f5bc8afd65b8
Jorge Martínez <28702884+jorgepiloto@users.noreply.github.com>
2022-06-14 17:12:08 +0200

    MAINT: modernize project layout (#115)
    * Remove configure scripts
    
    * Collect deps under requirements/ dir
    
    * Add tox.ini for automation
    
    * Update CI/CD to new tox workflow
    
    * Update .pre-commit-confgi.yml
    
    * Update to latest ansys-sphinx-theme==0.4.2

--- configure_venv.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-setlocal
-
-python -m venv venv
-pip install -r requirements_docs.txt
-venv\Scripts\activate.bat
--- configure_venv.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-python -m venv venv
-pip install -r requirements_docs.txt
-venv/activate
