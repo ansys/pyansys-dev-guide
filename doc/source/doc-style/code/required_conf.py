a36a27c190ab0358de4e803be91b6a147707bf8f
Roberto Pastor Muela <roberto.pastormuela@ansys.com>
2022-08-02 10:36:03 +0200

    Code style checks

--- doc/source/doc-style/code/required_conf.py
+++ doc/source/doc-style/code/required_conf.py
@@ -61,9 +61,9 @@ numpydoc_validation_checks = {
     "GL10",  # reST directives {directives} must be followed by two colons
     "SS01",  # No summary found
     "SS02",  # Summary does not start with a capital letter
-    "SS03", # Summary does not end with a period
+    "SS03",  # Summary does not end with a period
     "SS04",  # Summary contains heading whitespaces
-    "SS05", # Summary must start with infinitive verb, not third person
+    "SS05",  # Summary must start with infinitive verb, not third person
     "RT02",  # The first line of the Returns section should contain only the
     # type, unless multiple values are being returned"
 }
