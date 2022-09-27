c3f842875345fbe3c4379e2e0e323b1761a8a2bd
Revathyvenugopal162 <revathy.venugopal@ansys.com>
2022-09-27 10:53:20 +0200

    Resolve build error

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -58,8 +58,6 @@ extensions = [
 
 # -- Sphinx Gallery Options ---------------------------------------------------
 sphinx_gallery_conf = {
-    # convert rst to md for ipynb
-    "pypandoc": True,
     # path to your examples scripts
     "examples_dirs": ["examples"],  # ["../../examples/"],
     # path where to save gallery generated examples
