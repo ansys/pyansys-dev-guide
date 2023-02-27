1b2097592270820cd43772958f5896b3ba2e0dc8
Jorge Martinez <jorge.martinezgarrido@ansys.com>
2023-02-27 16:23:02 +0100

    CI: update version naming

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -17,7 +17,7 @@ from sphinx_gallery.sorting import FileNameSortKey
 project = "PyAnsys Developer's Guide"
 copyright = f"(c) {datetime.now().year} ANSYS, Inc. All rights reserved"
 author = "Ansys Inc."
-release = version = "0.3.dev0"
+release = version = datetime.now().strftime("%Y-%m-%d")
 
 html_logo = pyansys_logo_black
 html_theme = "ansys_sphinx_theme"
